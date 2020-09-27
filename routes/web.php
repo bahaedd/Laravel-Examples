f2e75125d97eb9e8bf5e1aae6d63e507e172f22d
bahaeddine <sihassi.bahaeddine@gmail.com>
2020-09-27 18:43:30 +0100

    Signature Pad Example

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SignatureController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/signature',[SignatureController::class,'index']);
+Route::post('/signature',[SignatureController::class,'upload'])->name('signature');
